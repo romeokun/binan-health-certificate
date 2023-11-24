69d8a3a851596aa0b31b8288a61be53c5f229bd2
romeokun <jromevan01@gmail.com>
2023-11-25 00:40:31 +0800

    can now load certificates from firestore

--- app/certificate/page.jsx
+++ app/certificate/page.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 import { SingleCertificate} from '@/components/certificateView/singleCertificate'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, Timestamp, query, orderBy } from "firebase/firestore";
+import { db } from "@/config/firebase";
 
-// to do: list all certificates
-function page() {
+// to do: list all certificates, add filters
+async function page() {
+  const q = query(collection(db, "certificates"), orderBy('created','desc'))
+  const querySnapshot = await getDocs(q);
+
   return (
     <div>
       certificates, print query
-      <input type="text" name="" id="" className='border'/>
-      <SingleCertificate />
-      items per page 10, 20, 50, 100
-      
+      <input type="text" name="" id="" className="border" />
+      {querySnapshot.docs?.map((Certificate, index) => {
+        return <SingleCertificate key={index} certificate={Certificate.data()} />;
+      })}
+      items per page 10, 25, 50, 100
     </div>
-  )
+  );
 }
 
-export default page
+
+
+export default page;
--- components/certificateView/singleCertificate.jsx
+++ components/certificateView/singleCertificate.jsx
@@ -13,8 +13,21 @@ import React from 'react'
 // PlaceOfWork: placeOfWork,
 
 
-export function SingleCertificate() {
+export function SingleCertificate({ certificate }) {
+  const { created, No, OrNo, DateIssued, Name, Occupation, Age, Sex, Nationality, PlaceOfWork} = certificate || {}
   return (
-    <div>created No OrNo DateIssued Name Occupation Age Sex Nationality PlaceofWork</div>
+    // <div>created No OrNo DateIssued Name Occupation Age Sex Nationality PlaceofWork</div>
+    <div>
+      {/* <span className="mx-4">{created}</span> */}
+      <span className="mx-4">{No}</span>
+      <span className="mx-4">{OrNo}</span>
+      {/* <span className="mx-4">{DateIssued}</span> */}
+      <span className="mx-4">{Name}</span>
+      <span className="mx-4">{Occupation}</span>
+      <span className="mx-4">{Age}</span>
+      <span className="mx-4">{Sex}</span>
+      <span className="mx-4">{Nationality}</span>
+      <span className="mx-4">{PlaceOfWork}</span>
+    </div>
   )
 }
