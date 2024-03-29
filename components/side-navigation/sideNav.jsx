a9ea3486716c80a8d2aac17d13b31714795d65b4
romeokun <jromevan01@gmail.com>
2024-03-29 12:30:01 +0800

    added logout button to side nav

--- components/side-navigation/sideNav.jsx
+++ components/side-navigation/sideNav.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState, useContext } from "react";
 
 import Link from "next/link";
@@ -7,10 +7,10 @@ import { Toggle } from "@/components/ui/toggle";
 import { Separator } from "@/components/ui/separator";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { AuthContext } from "../auth-provider";
+import { Button } from "@/components/ui/button";
 
 function sideNavigation() {
-
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, signout } = useContext(AuthContext);
 
   const [showNav, setShowNav] = useState(true);
   const handleToggleNav = () => {
@@ -35,18 +35,26 @@ function sideNavigation() {
           defaultStyle + (showNav ? " sticky " : " fixed translate-x-[-100%] ")
         }
       >
-        <div className="grid grid-cols-1 ">
+        <div className="h-full grid grid-rows-[min-content_1fr_min-content] ">
           <div>
-            <span className="block text-center text-white">
-              Binan Health Certificate
-            </span>
+            <div>
+              <span className="block text-center text-white text-lg font-bold my-2">
+                Binan Health Certificate
+              </span>
+            </div>
+            <div>
+              <span className="block text-center text-white font-semibold">
+                {currentUser?.displayName}
+              </span>
+            </div>
+            <div>
+              <span className="block text-center text-accent text-sm">
+                {currentUser?.email}
+              </span>
+            </div>
           </div>
-          <div>
-            <span className="block text-center text-white">
-              Name: {currentUser?.displayName}
-            </span>
-          </div>
-          <ul className="bg-slate-600">
+
+          <ul className="bg-slate-600 my-2 py-2">
             {navigations.map((nav) => {
               const samePage = pathname.includes(nav.link);
               return (
@@ -58,8 +66,13 @@ function sideNavigation() {
               );
             })}
           </ul>
+
+          <div className="grid place-content-center my-4">
+            <Button className="w-min" onClick={signout}>Logout</Button>
+          </div>
         </div>
       </div>
+
       <button
         className={
           "h-[50px] w-[50px] rounded-full fixed flex justify-center content-center flex-wrap bottom-2 shadow-black shadow-lg z-50 " +
@@ -77,7 +90,10 @@ function sideNavigation() {
 function NavigationButton({ link, children, isPressed }) {
   return (
     <Link href={link}>
-      <Toggle className="block w-9/12 mx-auto my-1 text-left" pressed={isPressed}>
+      <Toggle
+        className="block w-9/12 mx-auto my-1 text-left"
+        pressed={isPressed}
+      >
         {children}
       </Toggle>
     </Link>
