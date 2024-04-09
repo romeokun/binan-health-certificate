ce21bf2656cc03457060ac8f8bc7ac0c02686aa0
romeokun <jromevan01@gmail.com>
2024-04-09 11:22:53 +0800

    added protection in manage, default users can not access manage page

--- app/manage/layout.jsx
+++ app/manage/layout.jsx
@@ -1,6 +1,6 @@
 "use client";
 import SideNavigation from "@/components/side-navigation/sideNav";
-import React from "react";
+import { useContext, useEffect, useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -10,7 +10,9 @@ import {
 } from "@/components/ui/navigation-menu";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-
+import { AuthContext } from "@/components/auth-provider";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/config/firebase";
 
 const DashLink = ({ href, children }) => {
   const pathname = usePathname();
@@ -31,6 +33,24 @@ const DashLink = ({ href, children }) => {
 };
 
 function layout({ children }) {
+  const { currentUser, isLoading } = useContext(AuthContext);
+  const [state, setState] = useState("loading");
+
+  useEffect(() => {
+
+    getDoc(doc(db, "users", currentUser.uid)).then((user) => {
+      if (user) {
+        if (user.data().role == "admin") {
+          setState("authorized");
+        } else {
+          setState("unauthorized");
+        }
+      }
+    }).catch(error => {
+      setState("unauthorized")
+    });
+  }, []);
+
   return (
     <main className={"grid grid-cols-[min-content_1fr] bg-slate-100"}>
       <SideNavigation />
@@ -38,13 +58,19 @@ function layout({ children }) {
       <div className="grid place-items-center min-h-screen min-w-[500px]">
         <div className="bg-white my-6 min-h-[75vh] w-3/4 rounded shadow p-9">
           <h1 className="pl-4 pb-2 text-2xl font-semibold">Manage</h1>
-          <NavigationMenu>
-            <NavigationMenuList className="border p-2 rounded-sm shadow">
-              <DashLink href="/manage"> Logs </DashLink>
-              <DashLink href="/manage/users"> Users </DashLink>
-            </NavigationMenuList>
-          </NavigationMenu>
-          {children}
+          {state == "loading" && <>Loading</>}
+          {state == "unauthorized" && <>Unauthorized</>}
+          {state == "authorized" && (
+            <>
+              <NavigationMenu>
+                <NavigationMenuList className="border p-2 rounded-sm shadow">
+                  <DashLink href="/manage"> Logs </DashLink>
+                  <DashLink href="/manage/users"> Users </DashLink>
+                </NavigationMenuList>
+              </NavigationMenu>
+              {children}
+            </>
+          )}
         </div>
       </div>
     </main>
